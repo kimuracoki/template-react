5e7778dc172bc03ad191f9633625d39f2f773924
kimura.coki <kimura.coki@gmail.com>
2024-12-23 22:51:27 +0900

    Add page navigate

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,17 @@
 import { css } from "@emotion/react";
+import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
+    const navigate = useNavigate();
+    const handleClick = () => {
+        navigate('/about');
+    }
+
     return (
-        <h1 css={styles.h1}>This is Home</h1>
+        <>
+            <h1 css={styles.h1}>This is Home</h1>
+            <button onClick={handleClick}>to About</button>
+        </>
     );
 }
 
