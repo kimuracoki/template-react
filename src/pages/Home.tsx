47500911858ca131a7655675ba02d3f413b5dd6a
kimura.coki <kimura.coki@gmail.com>
2024-12-23 22:42:26 +0900

    Add emotion

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,13 @@
+import { css } from "@emotion/react";
+
 export const Home = () => {
     return (
-        <h1>This is Home</h1>
+        <h1 css={styles.h1}>This is Home</h1>
     );
+}
+
+const styles = {
+    h1: css`
+        text-align: center;
+    `
 }
