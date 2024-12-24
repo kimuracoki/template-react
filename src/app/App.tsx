9881a73c74408d9a8ea7cb974aea8d44ea238e05
Kimura Coki <kimura.coki@gmail.com>
2024-12-24 13:27:54 +0900

    Upd nomalize to CssBaseLine

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "../routes/Router";
+import { CssBaseline } from "@mui/material";
 
 export const App = () => {
 
   return (
     <>
+      <CssBaseline />
       <BrowserRouter>
         <Router />
       </BrowserRouter>
